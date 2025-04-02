d0aee5e67c586a3b14864ef511bb64207ac3dead
Danil Sheshenya <danil.sheshe@gmail.com>
2025-04-02 14:22:14 +0300

    refactor args and saving

--- regiondrag/run_eval.py
+++ regiondrag/run_eval.py
@@ -9,71 +9,124 @@ from PIL import Image
 from region_utils.drag import drag, get_drag_data, get_meta_data, drag_copy_paste, drag_id
 from region_utils.evaluator import DragEvaluator
 
-# Setting up the argument parser
-parser = argparse.ArgumentParser(description='Run the drag operation.')
-parser.add_argument('--data-dir', type=str) # 'drag_data/dragbench-dr' OR 'drag_data/dragbench-sr'
-parser.add_argument('--save-dir', type=str, default=None)
-parser.add_argument('--device', type=str, default='cuda')
-parser.add_argument('--method', type=str, default='regiondrag') # 'regiondrag' OR 'instantdrag' OR 'copy' OR 'id'
-parser.add_argument('--seed', type=int, default=42)
-parser.add_argument('--start-t', type=float, default=0.5)
-parser.add_argument('--end-t', type=float, default=0.2)
-parser.add_argument('--steps', type=int, default=20)
-parser.add_argument('--noise-scale', type=float, default=1.0)
-parser.add_argument('--disable-kv-copy', action='store_true')
-parser.add_argument('--n-edits', type=int, default=1)
-parser.add_argument('--skip-evaluation', action='store_true')
-args = parser.parse_args()
 
-print(args)
-print('Current commit:')
-os.system('git log -1')
+def get_args():
+    parser = argparse.ArgumentParser(description='Run the drag operation.')
+
+    parser.add_argument(
+        '--data-dir',
+        type=str,
+        required=True,
+        help="Path to the evaluation data directory. Should start with 'drag_data/'"
+    )
+
+    parser.add_argument(
+        '--save-dir',
+        type=str,
+        default=None,
+        help="Path to the save directory (i.e. name of the run). If not provided, results will not be saved."
+    )
+
+    parser.add_argument(
+        '--method',
+        type=str,
+        default='regiondrag',
+        choices=['regiondrag', 'guidance', 'instantdrag', 'copy', 'id'],
+        help="Method to use for the drag operation. Options: 'regiondrag', 'guidance', 'instantdrag', 'copy', 'id'"
+    )
+
+    parser.add_argument(
+        '--start-t',
+        type=float,
+        default=0.5,
+        help="Timestep to which the inversion is applied (e.g. 0.5 for 50% of the way through the diffusion process)"
+    )
+
+    parser.add_argument(
+        '--end-t',
+        type=float,
+        default=0.2,
+        help="Timestep at which the editing is terminated (e.g. 0.2 for 20% of the way through the diffusion process)"
+    )
+
+    parser.add_argument(
+        '--steps',
+        type=int,
+        default=20,
+        help="DDIM inversion steps"
+    )
+
+    parser.add_argument(
+        '--noise-scale',
+        type=float,
+        default=1.0,
+        help="If method is 'regiondrag', this is the weight of the noise applied in the inpainting region"
+    )
+
+    parser.add_argument(
+        '--disable-kv-copy',
+        action='store_true',
+        help="If passed, do not perform the key-value copy-paste operation in self-attention layers"
+    )
+
+    parser.add_argument(
+        '--skip-evaluation',
+        action='store_true',
+        help="If passed, do not perform the evaluation (saves time if you only want the output images)"
+    )
+
+    parser.add_argument('--device', type=str, default='cuda')
+    parser.add_argument('--seed', type=int, default=42)
+
+    args = parser.parse_args()
+    args.bench_name = args.data_dir.removeprefix('drag_data/').removesuffix('/') if args.data_dir != 'drag_data' else ""
+    args.save_dir = os.path.join('saved', args.save_dir, args.bench_name) if args.save_dir else None
+
+    # Validate arguments
+    assert args.data_dir.startswith('drag_data/'), "Data should lie in 'drag_data/'"
+    assert args.method in ['regiondrag', 'guidance', 'instantdrag', 'copy', 'id'], "Invalid method"
+    if args.save_dir and os.path.exists(args.save_dir):
+        raise FileExistsError(f"Save directory {args.save_dir} already exists")
+
+    # Log stuff
+    print(f"Using args: {args}")
+    print()
+    print('Current commit:')
+    os.system("git log -1")
+    print("*" * 100)
+
+    return args
+
+args = get_args()
 
 evaluator = DragEvaluator()
 all_distances = []; all_lpips = []; all_names = []
 
-save_dir = None if args.save_dir is None else args.save_dir.removesuffix("/")
-data_dir = args.data_dir.removesuffix("/")
-data_dirs = [dirpath for dirpath, dirnames, _ in os.walk(data_dir) if not dirnames]
-device = args.device
-method = args.method
-n_edits = args.n_edits
-
-bench = data_dir.split("/")[-1]
-
-start_t = args.start_t
-end_t = args.end_t
-steps = args.steps
-noise_scale = args.noise_scale
-seed = args.seed
+data_dirs = [dirpath for dirpath, dirnames, _ in os.walk(args.data_dir) if not dirnames]
 
 for data_path in tqdm(data_dirs):
     # Region-based Inputs for Editing
     drag_data = get_drag_data(data_path)
     ori_image = drag_data['ori_image'].copy()
 
-    if method in ('regiondrag', 'instantdrag'):
-        source = drag_data['source'].copy()
-        target = drag_data['target'].copy()
-        dir = target - source
+    if args.method in ('regiondrag', 'instantdrag', 'guidance'):
+        if args.method == 'regiondrag':
+            method = 'Encode then CP'
+        elif args.method == 'instantdrag':
+            method = 'InstantDrag'
+        elif args.method == 'guidance':
+            method = 'guidance'
 
-        for i in range(n_edits):
-            source_i = np.round(source + dir * i / n_edits).astype(np.int64)
-            target_i = np.round(source + dir * (i + 1) / n_edits).astype(np.int64)
-            drag_data['source'] = source_i
-            drag_data['target'] = target_i
+        out_image = drag(drag_data, args.steps, args.start_t, args.end_t, args.noise_scale, args.seed,
+                        progress=gr.Progress(), device=args.device, disable_kv_copy=args.disable_kv_copy,
+                        method='Encode then CP' if args.method == 'regiondrag' else 'InstantDrag')
+    elif args.method == 'copy':
+        out_image = drag_copy_paste(drag_data, device=args.device) 
+    elif args.method == 'id':
+        out_image = drag_id(drag_data, device=args.device)
 
-            out_image = drag(drag_data, steps, start_t, end_t, noise_scale, seed,
-                            progress=gr.Progress(), device=device, disable_kv_copy=args.disable_kv_copy,
-                            method='Encode then CP' if method == 'regiondrag' else 'InstantDrag')
-            drag_data['ori_image'] = out_image 
-    elif method == 'copy':
-        out_image = drag_copy_paste(drag_data, device=device) 
-    elif method == 'id':
-        out_image = drag_id(drag_data, device=device)
-
-    if save_dir is not None:
-        save_name = f"{save_dir}/{data_path.removeprefix('drag_data/')}.png"
+    if args.save_dir is not None:
+        save_name = os.path.join(args.save_dir, data_path.removeprefix(f'drag_data/{args.bench_name}/') + '.png')
         os.makedirs(os.path.dirname(save_name), exist_ok=True)
         Image.fromarray(out_image).save(save_name)
 
@@ -87,16 +140,16 @@ for data_path in tqdm(data_dirs):
         all_names.append(data_path)
 
 
-if all_distances:
+if not args.skip_evaluation:
     mean_dist = torch.tensor(all_distances).mean().item() * 100
     mean_lpips = torch.tensor(all_lpips).mean().item() * 100
     print(f'MD: {mean_dist:.4f}\nLPIPS: {mean_lpips:.4f}\n')
 
-    if save_dir is not None:
+    if args.save_dir is not None:
         md = np.array(all_distances)
         lpips = np.array(all_lpips)
 
-        filepath = f"{save_dir}/{bench}/metrics.npz"
+        filepath = os.path.join(args.save_dir, 'metrics.npz')
         np.savez(
             filepath, md=md, lpips=lpips, names=all_names,
             mean_md=np.array(mean_dist), mean_lpips=np.array(mean_lpips)
