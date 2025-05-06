3daec778fd0a009d43dccf4015625e90d4388c74
Danil Sheshenya <danil.sheshe@gmail.com>
2025-05-06 14:49:13 +0300

    GAMECHANGER

--- regiondrag/region_utils/energy.py
+++ regiondrag/region_utils/energy.py
@@ -9,8 +9,8 @@ def get_cosine_local_similarity_function():
     def similarity_function(feature_map, inv_feature_map, source, target):
         sim = (
             torch.nn.functional.cosine_similarity(
-                feature_map[0, :, source[:, 1], source[:, 0]],
-                inv_feature_map[0, :, target[:, 1], target[:, 0]],
+                feature_map[0, :, target[:, 1], target[:, 0]],
+                inv_feature_map[0, :, source[:, 1], source[:, 0]],
                 dim=1
             )
             + 1.0
@@ -21,8 +21,8 @@ def get_cosine_local_similarity_function():
 
 def get_cosine_global_similarity_function():
     def similarity_function(feature_map, inv_feature_map, source, target):
-        v1 = feature_map[0, :, source[:, 1], source[:, 0]].mean(dim=1)
-        v2 = inv_feature_map[0, :, target[:, 1], target[:, 0]].mean(dim=1)
+        v1 = feature_map[0, :, target[:, 1], target[:, 0]].mean(dim=1)
+        v2 = inv_feature_map[0, :, source[:, 1], source[:, 0]].mean(dim=1)
         sim = torch.nn.functional.cosine_similarity(v1, v2, dim=0)
         return sim
     return similarity_function
