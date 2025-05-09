90f1bdc614480fd6916156db1fff4bd6ebc7003c
Danil Sheshenya <danil.sheshe@gmail.com>
2025-05-09 20:02:34 +0300

    update env

---
+++ make_script.py
@@ -0,0 +1,28 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--exp-name', type=str, default='baseline')
+
+args, unknown = parser.parse_known_args()
+add_args = " ".join(unknown)
+
+print("Passed to run_eval.py:", unknown)
+
+exp_name = args.exp_name
+script_name = f"run-{exp_name}.sh"
+
+script = \
+f"""#!/bin/bash
+set -e
+
+if [ -z "$CUDA_VISIBLE_DEVICES" ]; then
+  echo "Error: CUDA_VISIBLE_DEVICES is not set. Please set this environment variable to specify which GPUs to use." >&2
+  exit 1
+fi
+
+python3 regiondrag/run_eval.py --data-dir "drag_data/dragbench-dr" --save-dir "{exp_name}" {add_args}
+python3 regiondrag/run_eval.py --data-dir "drag_data/dragbench-sr" --save-dir "{exp_name}" {add_args}
+"""
+
+with open(script_name, "w") as f:
+    f.write(script)
